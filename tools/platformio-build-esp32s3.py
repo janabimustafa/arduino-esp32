d76958222584aa6d59785ededbfb9974a75b6743
Mustafa Taleb <mustafa@mustafataleb.com>
2023-09-07 13:01:23 -0700

    updated fatfs

--- tools/platformio-build-esp32s3.py
+++ tools/platformio-build-esp32s3.py
@@ -327,7 +327,7 @@ env.Append(
         "UNITY_INCLUDE_CONFIG_H",
         "WITH_POSIX",
         "_GNU_SOURCE",
-        ("IDF_VER", '\\"v4.4.5-355-g1f00708c84\\"'),
+        ("IDF_VER", '\\"v4.4.5-358-g17892b9daf\\"'),
         "ESP_PLATFORM",
         "_POSIX_READER_WRITER_LOCKS",
         "ARDUINO_ARCH_ESP32",
